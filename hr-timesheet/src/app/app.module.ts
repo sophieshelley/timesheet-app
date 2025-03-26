d0eb77cd578b971c57260e4a3496687f03eec68c
sophieshelley <sophiashelley17@gmail.com>
2025-03-26 10:22:10 -0600

    completed unit 3

--- hr-timesheet/src/app/app.module.ts
+++ hr-timesheet/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { DepartmentsComponent } from './components/departments/departments.component';
@@ -8,6 +7,7 @@ import { TimesheetComponent } from './components/timesheet/timesheet.component';
 import { AnalyticsComponent } from './components/analytics/analytics.component';
 import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
 import { AnalyticsTableComponent } from './components/analytics-table/analytics-table.component';
+import { MaterialModule } from './modules/material.module';
 
 @NgModule({
   declarations: [
@@ -20,9 +20,12 @@ import { AnalyticsTableComponent } from './components/analytics-table/analytics-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
