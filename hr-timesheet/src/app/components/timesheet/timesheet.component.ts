caa23d5c5de3b1d6dad209af3bd72b1f2a86a564
sophieshelley <sophiashelley17@gmail.com>
2025-04-21 14:08:44 -0600

    completed unit 8 part 2

--- hr-timesheet/src/app/components/timesheet/timesheet.component.ts
+++ hr-timesheet/src/app/components/timesheet/timesheet.component.ts
@@ -12,7 +12,7 @@ import { Employee } from '../../interfaces/employee';
   styleUrl: './timesheet.component.scss'
 })
 
-export class TimesheetComponent implements OnInit{
+export class TimesheetComponent implements OnInit {
   departments: Department[] | undefined;
   department: Department | undefined;
   employeeNameFC = new FormControl('', this.nameValidator());
@@ -33,38 +33,47 @@ export class TimesheetComponent implements OnInit{
 
   addEmployee(): void {
     if (this.employeeNameFC.value) {
-        this.employeeId++;
+      this.employeeId++;
 
-        this.employees.push({
-            id: this.employeeId.toString(),
-            departmentId: this.department?.id,
-            name: this.employeeNameFC.value,
-            payRate: Math.floor(Math.random() * 50) + 50,
-            monday: 0,
-            tuesday: 0,
-            wednesday: 0,
-            thursday: 0,
-            friday: 0,
-            saturday: 0,
-            sunday: 0
-        });
+      this.employees.push({
+        id: this.employeeId.toString(),
+        departmentId: this.department?.id,
+        name: this.employeeNameFC.value,
+        payRate: Math.floor(Math.random() * 50) + 50,
+        monday: 0,
+        tuesday: 0,
+        wednesday: 0,
+        thursday: 0,
+        friday: 0,
+        saturday: 0,
+        sunday: 0
+      });
 
-        this.employeeNameFC.setValue('');
+      this.employeeNameFC.setValue('');
     }
-}
-  
+  }
+
   nameValidator(): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } | null => {
-        let error = null;
-        if (this.employees && this.employees.length) {
-            this.employees.forEach(employee => {
-                if (employee.name.toLowerCase() === control.value.toLowerCase()) {
-                    error = {duplicate: true};
-                }
-            });
-        }
-        return error;
+      let error = null;
+      if (this.employees && this.employees.length) {
+        this.employees.forEach(employee => {
+          if (employee.name.toLowerCase() === control.value.toLowerCase()) {
+            error = { duplicate: true };
+          }
+        });
+      }
+      return error;
     };
+  }
+
+  getTotalHours(employee: Employee): number {
+    return employee.monday + employee.tuesday + employee.wednesday
+      + employee.thursday + employee.friday + employee.saturday + employee.sunday;
+  }
+
+  deleteEmployee(index: number): void {
+    this.employees.splice(index, 1);
 }
 
 }
