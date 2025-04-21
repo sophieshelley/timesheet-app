e1ef4ce074d0b3cc29ce428f07651a7c05b635e6
sophieshelley <sophiashelley17@gmail.com>
2025-04-21 15:38:33 -0600

    completed unit 11

--- hr-timesheet/src/app/components/departments/departments.component.ts
+++ hr-timesheet/src/app/components/departments/departments.component.ts
@@ -23,7 +23,9 @@ export class DepartmentsComponent implements OnInit{
   ) { }
 
   ngOnInit(): void {
-    this.departments = this.departmentsService.departments;
+    this.departmentsService.getDepartments().subscribe(departments => {
+      this.departments = departments;
+  });
   }
   goToDepartment(departmentId: string): void {
     this.router.navigate(['./timesheet', {id: departmentId}]);
