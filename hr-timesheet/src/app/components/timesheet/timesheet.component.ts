1ca34ef56d4df466e65030f40353354985ba9073
sophieshelley <sophiashelley17@gmail.com>
2025-04-21 19:30:02 -0600

    completed unit 13 part 2

--- hr-timesheet/src/app/components/timesheet/timesheet.component.ts
+++ hr-timesheet/src/app/components/timesheet/timesheet.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Department } from '../../interfaces/department';
 import { DepartmentsService } from '../../services/departments.service';
 import { FormControl, ValidatorFn, AbstractControl } from '@angular/forms';
 import { Employee } from '../../interfaces/employee';
+import { EmployeeService } from '../../services/employee.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-timesheet',
@@ -11,9 +14,8 @@ import { Employee } from '../../interfaces/employee';
   templateUrl: './timesheet.component.html',
   styleUrl: './timesheet.component.scss'
 })
-
 export class TimesheetComponent implements OnInit {
-  departments: Department[] | undefined;
+  $departments: Observable<Department[]> | undefined;
   department: Department | undefined;
   employeeNameFC = new FormControl('', this.nameValidator());
   employees: Employee[] = [];
@@ -22,21 +24,25 @@ export class TimesheetComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private departmentsService: DepartmentsService
-
+    private departmentsService: DepartmentsService,
+    private employeeService: EmployeeService,   // <-- ADD THIS
+    private router: Router                      // <-- ADD THIS
   ) { }
 
   ngOnInit(): void {
-    this.departments = this.departmentsService.departments;
-    this.department = this.departments.find(department => department.id === this.route.snapshot.params['id']);
+    this.$departments = this.departmentsService.getDepartments();
+  
+    this.$departments.subscribe(x => {
+      this.department = x.find(dept => dept.id === this.route.snapshot.params['id']);
+    });
   }
+  
 
   addEmployee(): void {
     if (this.employeeNameFC.value) {
       this.employeeId++;
 
       this.employees.push({
-        id: this.employeeId.toString(),
         departmentId: this.department?.id,
         name: this.employeeNameFC.value,
         payRate: Math.floor(Math.random() * 50) + 50,
@@ -74,7 +80,15 @@ export class TimesheetComponent implements OnInit {
 
   deleteEmployee(index: number): void {
     this.employees.splice(index, 1);
-}
+  }
 
+  // --- ADD THIS ---
+  submit(): void {
+    this.employees.forEach(employee => {
+      this.employeeService.saveEmployeeHours(employee);
+    });
+
+    this.router.navigate(['./departments']);
+  }
 }
 
---
+++ hr-timesheet/src/app/services/employee.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Employee } from '../interfaces/employee';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  private employeeHoursCollection: AngularFirestoreCollection<Employee>;
+
+  constructor(private db: AngularFirestore) {
+    // Initialize collection reference in the constructor where injection context is available
+    this.employeeHoursCollection = this.db.collection('employee-hours');
+  }
+
+  saveEmployeeHours(employee: Employee): Promise<any> {
+    employee.departmentId = employee.departmentId || '0';
+    console.log('Saving employee hours:', employee);
+    // Use the pre-initialized collection reference
+    return this.employeeHoursCollection.add(employee);
+  }
+}
